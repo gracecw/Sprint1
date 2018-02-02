1dd28273717309d973b33dff291a3225e7d7db4e
Jing Song <jingsong@Jings-MacBook-Pro.local>
2018-02-01 18:50:35 -0800

    tweak

--- process.py
+++ process.py
@@ -2,20 +2,21 @@ import os, json, sys
 
 dataDir = '/srv/runme/'
 prefix = sys.argv[1]
+
 f = open(dataDir+prefix+'.txt', 'w')
 
 for file in os.listdir(dataDir):
-    if file.startswith(prefix) and file.endswith('json'):
-        json_str = open(dataDir+file).read()
-        json_dict = json.loads(json_str)
+    if file.startswith(prefix):
+        json_strs = open(dataDir+file).readlines()
+        for json_str in json_strs:
+            try:
+                json_dict = json.loads(json_str)
+                name = json_dict['name']
+                age = json_dict['prop']['age']
+                str1 = str(name)+'\t'+str(age)+'\n'
+                f.write(str1)
 
-        try:
-            name = json_dict['name']
-            age = json_dict['prop']['age']
-            str1 = str(name)+'\t'+str(age)+'\n'
-            f.write(str1)
-
-        except:
-            continue
+            except:
+                continue
 
 f.close()
