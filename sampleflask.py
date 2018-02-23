e70ef4cf6efeb76ce9f0afbf9ff60eb5ca69c29f
gracecw <cwang98@dons.usfca.edu>
2018-02-23 09:58:54 -0800

    update sampleflask

--- sampleflask.py
+++ sampleflask.py
@@ -15,4 +15,14 @@ def foo():
     return "This is test page."
 
 
+@app.route("/request/", methods=['GET','POST'])
+def print_request():
+    try:
+        mydata = request.json # will be 
+        return "Thanks. Your data is %s" % mydata
+        return 'JSON posted'
+    except:
+        return 'No Json Received'
+
+
 app.run('0.0.0.0', port=8080, debug = True)
