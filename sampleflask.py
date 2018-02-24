fe0c88e06804ca017f813cf1c0b9658681ac5062
gracecw <cwang98@dons.usfca.edu>
2018-02-23 22:44:09 -0800

    test

--- sampleflask.py
+++ sampleflask.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask import request
 import sys
-
-
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -24,16 +22,14 @@ def get_raw_request():
     # retrieve the incoming request data as string
     raw_request = request.data
     line = str(raw_request).replace("\n", " ")
-
-    # 'a' mode to append to the existing Raw.txt
-    app.logger.info(line)
-    return "Json posted"
+    with open(/srv/runme/groupb/log.txt, 'a+') as f:
+        f.write(line+'\n')
+    f.close()
+    return "Received"
 
 
 if __name__ == '__main__':
     handler = TimedRotatingFileHandler('/srv/runme/groupb/log.txt', when='S', interval=30)
-    handler.setLevel(logging.INFO)
-    app.logger.setLevel(logging.INFO)
+    #handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
-    
     app.run('0.0.0.0', port=8080, debug = True)
