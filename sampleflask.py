7c704d3f69467c29f9cd59720d9b28ef6c10f3b3
gracecw <cwang98@dons.usfca.edu>
2018-02-23 23:50:03 -0800

    update

--- sampleflask.py
+++ sampleflask.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask import request
 import sys
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 
 
 app = Flask(__name__)
+app.logger.setLevel(logging.NOTSET)
 
 @app.route("/hello/<name>")
 def f(name):
@@ -22,14 +23,12 @@ def get_raw_request():
     # retrieve the incoming request data as string
     raw_request = request.data
     line = str(raw_request).replace("\n", " ")
-    with open(/srv/runme/groupb/log.txt, 'a+') as f:
-        f.write(line+'\n')
-    f.close()
+    app.logger.info('line')
     return "Received"
 
 
 if __name__ == '__main__':
-    handler = TimedRotatingFileHandler('/srv/runme/groupb/log.txt', when='S', interval=30)
-    #handler.setLevel(logging.INFO)
+    handler = TimedRotatingFileHandler('/srv/runme/groupb/log.txt', when='S', interval=10)
+    handler.setLevel(logging.NOTSET)
     app.logger.addHandler(handler)
     app.run('0.0.0.0', port=8080, debug = True)
