bed4f7df42279a1592199196afbbc0c42f6be9cb
gracecw <cwang98@dons.usfca.edu>
2018-02-23 20:30:12 -0800

    rename and updated sample

--- sampleflask.py
+++ sampleflask.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request
-
+import sys
 
 app = Flask(__name__)
 
@@ -10,24 +10,32 @@ def f(name):
     return "Hello %s!\n" % name
 
 
-@app.route("/")
-def foo():
-    return "This is test page."
+#@app.route("/")
+#def foo():
+#    return "This is test page."
+
+@app.route("/", methods=['POST'])
+
+#def print_request():
+#    try:
+#        mydata = request.json # will be 
+#       return "Thanks. Your data is %s" % mydata
+#       return 'JSON posted'
+#   except:
+#       return 'No Json Received'
+
+def get_raw_request():
+    # retrieve the incoming request data as string
+    raw_request = request.data
+    line = str(raw_request).replace("\n", " ")
+
+    # 'a' mode to append to the existing Raw.txt
+    with open('/srv/runme/groupa/Raw.txt', 'a+') as f:
+        f.write(line+'\n')
+    f.close()
+    return "Json posted"
 
 
-@app.route("/request/", methods=['GET','POST'])
-def print_request():
-    mydata = request.json
-    if mydata != None:
-        dataDir = '/srv/runme/groupa/'
-        f = open(dataDir+'Raw.txt', 'w')
-        f.write(mydata)
-        f.close()
-        return 'JSON posted'
-    
-    else:
-        return 'No Json Received.'
-
-
-app.run('0.0.0.0', port=8080, debug = True)
-
+if __name__ == '__main__':
+    #i = sys.argv.index('server:app')
+    app.run('0.0.0.0', port=8080, debug = True)
--- sprint_flask.py
+++ sprint_flask.py
@@ -8,15 +8,16 @@ prefix =  sys.argv[1]
 
 app = Flask(__name__)
 
-@app.route('/request/', methods = ['POST'])
+@app.route('/', methods = ['POST'])
 def get_raw_request():
     # retrieve the incoming request data as string
     raw_request = request.data
     line = str(raw_request).replace("\n", " ")
 
     # 'a' mode to append to the existing Raw.txt
-    with open(dataDir+prefix+'Raw.txt', 'a') as f:
+    with open(dataDir+prefix+'\Raw.txt', 'a+') as f:
         f.write(line+'\n')
     f.close()
+    return "Received"
 
 app.run(host='0.0.0.0', port=8080)
