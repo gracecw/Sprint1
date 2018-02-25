415e2f5fdc570936af6dd30a23038a1f27a03d6b
gracecw <cwang98@dons.usfca.edu>
2018-02-24 19:17:52 -0800

    add test raw rotator.py

---
+++ rawrotator.py
@@ -0,0 +1,27 @@
+import time
+import os
+from datetime import datetime
+import sys
+
+prefix =  sys.argv[1]
+
+def rotate(prefix):
+    #get the original file
+    origin = '/srv/runme/%s/Raw.txt' %(prefix)
+
+    #get current time
+    t =datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
+    #get the name for renaming
+    fname ='/srv/runme/%s/rawlog_%s.txt'%(prefix, t)
+
+
+    #rename the file
+    os.rename(origin, fname)
+
+    #if not created by flask, create new
+    if not os.path.exists(origin):
+        with open(origin, 'w'): pass
+    
+    return "File rotated!"
+
