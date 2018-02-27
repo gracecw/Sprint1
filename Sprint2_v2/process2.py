b9dee02ddaf3ac0c50ef3d13933e427b5d04c43c
Jing Song <jsong29@dons.usfca.edu>
2018-02-27 00:23:44 -0800

    write to proc

--- Sprint2_v2/process2.py
+++ Sprint2_v2/process2.py
@@ -1,4 +1,5 @@
 import os, json, sys
+import time
 
 timestamp = sys.argv[1]
 prefix = sys.argv[2]
@@ -7,7 +8,7 @@ dataDir = '/srv/runme/' + prefix + '/'
 proc_log_name = "procraw_%s.txt" % (timestamp)
 raw_log_name = "rawlog_%s.txt" % (timestamp)
 
-f = open(dataDir + proc_log_name, 'w')
+f = open(dataDir + prefix + 'proc', 'a+')
 
 def dict_raise_on_duplicates(ordered_pairs):
     """Reject duplicate keys."""
@@ -19,20 +20,25 @@ def dict_raise_on_duplicates(ordered_pairs):
             d[k] = v
     return d
 
+while True:
+    if not os.path.exists(dataDir + prefix + "/flock_proc"):
+        break
+        print "Sorry, the server is busy now..."
+        time.sleep(0.001)
 
-json_strs = open(dataDir + raw_log_name).readlines()
+    json_strs = open(dataDir + raw_log_name).readlines()
 
-for json_str in json_strs:
-    try:
-        json_dict = json.loads(json_str, object_pairs_hook=dict_raise_on_duplicates)
-        name = json_dict['name']
-        age = json_dict['prop']['age']
-        if int(age) < 0 or len(name) == 0:
-                                            continue
-        str1 = str(name) + '\t' + str(age) + '\n'
-        f.write(str1)
+    for json_str in json_strs:
+        try:
+            json_dict = json.loads(json_str, object_pairs_hook=dict_raise_on_duplicates)
+            name = json_dict['name']
+            age = json_dict['prop']['age']
+            if int(age) < 0 or len(name) == 0:
+                                                continue
+            str1 = str(name) + '\t' + str(age) + '\n'
+            f.write(str1)
 
-    except:
-        continue
+        except:
+            continue
 
 f.close()
