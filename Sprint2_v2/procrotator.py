c3c43412a1edcc86bcd0c6b5b74323331c7c6e0f
gracecw <cwang98@dons.usfca.edu>
2018-02-27 00:19:36 -0800

    update v2

---
+++ Sprint2_v2/procrotator.py
@@ -0,0 +1,34 @@
+import os
+from datetime import datetime
+import sys
+
+prefix = sys.argv[1]
+
+
+def rotate(prefix):
+    # get the original file
+    origin = '/srv/runme/%s/Proc.txt' % (prefix)
+    
+    # get current time
+    global t
+    t = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    
+    # get the name for renaming
+    fname = '/srv/runme/%s/proc_log_%s.txt' % (prefix, t)
+    
+    # create lock file to avoid simultaneous accessing
+    os.system('mkdir /srv/runme/%s/flock_proc')
+    
+    # rename the raw file
+    os.rename(origin, fname)
+    
+    # create new raw file
+    with open(origin, 'w'):pass
+    
+    #remove the lock after the process is completed
+    os.system('rm -f /srv/runme/%s/flock_proc')
+    return "Proc Log rotated!"
+
+#If Raw.txt is not empty, rotate it; call process2.py to process it.
+if os.stat('/srv/runme/%s/Proc.txt' % (prefix)).st_size != 0:
+    rotate(prefix)
---
+++ Sprint2_v2/server.py
@@ -0,0 +1,40 @@
+import sys
+from flask import Flask, request
+import os
+import time
+
+dataDir = '/srv/runme/'
+prefix = sys.argv[1]
+
+app = Flask(__name__)
+
+# make prefix dir if it does not exist
+if not os.path.exists(dataDir + prefix):
+    print("%s does not exists, creating now" % (prefix))
+    os.system('mkdir %s' % (dataDir + prefix))
+
+
+@app.route('/', methods=['POST'])
+def get_raw_request():
+    
+    # check for lock file
+    while True:
+        if not os.path.exists(dataDir + prefix + "/flock"):
+            break
+        print "Sorry, the server is busy now..."
+        time.sleep(0.001)
+    
+    # retrieve the incoming request data as string
+    raw_request = request.data
+    line = str(raw_request).replace("\n", " ")
+    
+    # 'a' mode to append to the existing Raw.txt
+    with open(dataDir + prefix + '/Raw.txt', 'a+') as f:
+        f.write(line + '\n')
+    f.close()
+    
+    return "Received"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
